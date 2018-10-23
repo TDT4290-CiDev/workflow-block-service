3c0765b4d5d3c1a2ac377e5176cadd4d6bb5a700
sjrodahl <son.rodahl@gmail.com>
2018-10-23 20:45:06 +0200

    Restructured with helper functions

--- src/tests/test_example_block.py
+++ src/tests/test_example_block.py
@@ -2,11 +2,11 @@ import unittest
 import copy
 
 from src import block_manager
-
 from src.main import app
 
 example_input = {'params': {'param1': 'test', 'param2': 1}}
 example_suspend = {'params': {'param1': 'suspend', 'param2': 1}}
+
 
 
 class ExampleTests(unittest.TestCase):
@@ -14,6 +14,15 @@ class ExampleTests(unittest.TestCase):
     def setUp(self):
         self.blocks = block_manager.blocks
         self.app = app.test_client()
+
+    def execute_example(self, param1=None, param2=None):
+        input = {'params': {}}
+        if param1 is not None:
+            input['params']['param1'] = param1
+        if param2 is not None:
+            input['params']['param2'] = param2
+        return self.app.post('/example', json = input)
+
 
     def test_discovery(self):
         response = self.app.get('/discovery')
@@ -27,32 +36,27 @@ class ExampleTests(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_example_execution(self):
-        response = self.app.post('/example', json=example_input)
+        response = self.execute_example('test', 1)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Hello', response.data)
         self.assertIn(b'world', response.data)
 
     def test_missing_param(self):
-        data = {'params': {'param1': 'test missing param2'}}
-        response = self.app.post('/example', json=data)
+        response = self.execute_example('test_missing_param2')
         self.assertEqual(response.status_code, 400)
         self.assertIn(b'Missing parameter param2', response.data)
 
     def test_wrong_param_type(self):
-        data = copy.deepcopy(example_input)
-        data['params']['param1'] = 5
-        response = self.app.post('/example', json=data)
+        response = self.execute_example(1, 1)
         self.assertEqual(response.status_code, 400)
         self.assertIn(b'Invalid type for parameter param1', response.data)
 
-        data = copy.deepcopy(example_input)
-        data['params']['param2'] = 'string, not integer'
-        response = self.app.post('/example', json=data)
+        response = self.execute_example('test', 'string_not_integer')
         self.assertEqual(response.status_code, 400)
         self.assertIn(b'Invalid type for parameter param2', response.data)
 
     def test_example_suspension(self):
-        response = self.app.post('/example', json=example_suspend)
+        response = self.execute_example('suspend', 1)
         resp_data = response.get_json()
         susp_count = resp_data['state']['suspend_count']
         self.assertEqual(response.status_code, 200)
@@ -62,14 +66,11 @@ class ExampleTests(unittest.TestCase):
         self.assertIn(b'"type":"suspend"', response.data)
 
     def test_example_resuming(self):
-        suspend_response = self.app.post('/example', json=example_suspend)
+        suspend_response = self.execute_example('suspend', 1)
         self.assertEqual(suspend_response.status_code, 200)
         data = suspend_response.get_json()
         data['params'] = {'param1': 'test_resume'}
         resume_response = self.app.post('/example/resume', json=data)
         self.assertEqual(resume_response.status_code, 200)
         print(resume_response.data)
-
-
-
 
