1bf83a126aff4cdaec86c7ea422090ccb89df4dd
Eivind Lie Andreassen <ela4991@gmail.com>
2018-11-13 14:54:55 +0100

    Remove duplicate test

--- src/main.py
+++ src/main.py
@@ -82,9 +82,6 @@ def execute_block(block_name):
 
         cleaned_params[name] = body['params'][name]
 
-    if is_resuming and 'state' not in body:
-        return 'State must be sent when resuming', HTTPStatus.BAD_REQUEST
-
     try:
         if is_resuming:
             result = block.resume(body['state'], cleaned_params)
